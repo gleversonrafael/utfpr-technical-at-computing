954928c38075c32d06cd3a063759509300e3851d
gleversonrafael <gleverson_rafael@hotmail.com>
2025-11-14 09:57:00 -0300

    updated crud

---
+++ crud-generator-14-11/teste/Boletim.js
@@ -0,0 +1,10 @@
+class Boletim {
+   constructor (id,materia,nota1,nota2, posicaoNaLista) {
+          this.id = id;
+          this.materia = materia;
+          this.nota1 = nota1;
+          this.nota2 = nota2;
+
+          this.posicaoNaLista = posicaoNaLista;
+   }
+}
